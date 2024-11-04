9ea7711aa6552ae5bcb35bfda27adae3074d7ad7
Jonah Paten <jonah@sharedusemobilitycenter.org>
2024-11-04 12:09:16 -0800

    remove unused files, move gbfs data object to wip

--- MobilityHubDataObjects/__init__.py
+++ MobilityHubDataObjects/__init__.py
@@ -2,10 +2,8 @@ from .FTAFacilityInventoryDataObject import *
 from .OSMDataObject import *
 from .SpatialDataObject import *
 from .AFDCApiDataObject import *
-from .APIDataObject import *
 from .GTFSDataObject import *
 from .constants import *
-from .IPCDDataObject import *
 from .EJScreenDataObject import *
 from .GTFSFeedWrapper import *
 from .CityBikesDataObject import *
@@ -13,15 +11,12 @@ from .scoreFunctions import *
 from .ScoreWrapper import *
 
 __all__ = [
-    "APIDataObject",
     "AFDCApiDataObject",
-    "constants",
     "SpatialDataObject",
     "EJScreenDataObject",
     "GTFSFeedWrapper",
     "FTAFacilityInventoryDataObject",
     "GTFSDataObject",
-    "IPCDDataObject",
     "OSMDataObject",
     "CityBikesDataObject",
     "ScoreWrapper"
